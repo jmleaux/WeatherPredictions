f88b30189e76ec0d94a631e8b94cd41f32bc3687
jmleaux <chip@jmlowe.net>
2017-03-26 20:27:17 +0300

    UI built

---
+++ WeatherPrediction/WeatherVC.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherVC.swift
+//  WeatherPrediction
+//
+//  Created by J. M. Lowe on 3/24/17.
+//  Copyright © 2017 JMLeaux LLC. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+
+}
+
