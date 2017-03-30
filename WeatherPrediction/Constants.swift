3b4852e3036323e375fa8b1c82aee173ead0d0c8
jmleaux <chip@jmlowe.net>
2017-03-30 20:06:43 +0300

    starting the api call lesson

---
+++ WeatherPrediction/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  WeatherPrediction
+//
+//  Created by J. M. Lowe on 3/30/17.
+//  Copyright © 2017 JMLeaux LLC. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "b65b7255aeaf3e0d373c29f20cd235e6"
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-35\(LONGITUDE)122\(APP_ID)\(API_KEY)"
+//print(CURRENT_WEATHER_URL)
