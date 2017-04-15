d1c8f8d1c5675f847b61487df6d48555cdc711f4
jmleaux <chip@jmlowe.net>
2017-04-15 16:46:17 +0300

    end of lecture 93

--- WeatherPrediction/Constants.swift
+++ WeatherPrediction/Constants.swift
@@ -10,8 +10,8 @@ import Foundation
 
 let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 let LATITUDE = "lat="
-let LONGITUDE = "&lon="
-let APP_ID = "&appid="
+let LONGITUDE = "lon="
+let APP_ID = "appid="
 let API_KEY = "b65b7255aeaf3e0d373c29f20cd235e6"
-let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-35\(LONGITUDE)122\(APP_ID)\(API_KEY)"
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-35&\(LONGITUDE)122&\(APP_ID)\(API_KEY)"
 //print(CURRENT_WEATHER_URL)
